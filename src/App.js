64eaf2382c98be13e04b4a10e4c82a34f1905d13
lwlz <lwlz.inc@gmail.com>
2020-07-01 01:49:26 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Container from '@material-ui/core/Container';
+import NavBar from './components/Navigation/NavBar';
+import Home from './components/Home/Home';
+import Articles from './components/Articles/Articles';
+import Basket from './components/Basket/Basket';
+import * as articlesActions from './actions/articlesActions';
+import * as counterActions from './actions/counterActions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './sass/App.scss';
+
+class App extends PureComponent {
+    render() {
+        const histories = this.props.history;
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <NavBar />
+                <Container maxWidth='lg' style={{ marginTop: 70 }}>
+                    <ConnectedRouter history={histories}>
+                        <Switch>
+                            <Route exact path='/' render={() => <Redirect to='/home' />} />
+                            <Route path='/home' component={Home} />
+                            <Route exact path='/articles' component={Articles} />
+                            <Route exact path='/basket' component={Basket} />
+                        </Switch>
+                    </ConnectedRouter>
+                </Container>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    counterRed: state.counterReducer,
+    articlesRed: state.articlesReducer
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    counterActions: bindActionCreators(counterActions, dispatch),
+    articlesActions: bindActionCreators(articlesActions, dispatch)
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import history from './utils/history';
+import { configureStore } from './store/configureStore';
+import './sass/App.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// eslint-disable-next-line
+const initialState = window.__REDUX_STATE__ || {};
+const store = configureStore(initialState);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = (Component) => {
+    render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <Component history={history} />
+            </Provider>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+renderApp(App);
